3e7bb46a45889d0ffa54c5da6d8ce2c3b46861bc
Craig Cooper <Craig.Cooper@ibm.com>
2022-11-01 16:58:02 -0500

    Version 0.1.11

---
+++ README.md
@@ -0,0 +1,19 @@
+# IBM Techzone (Homebrew)
+
+[Homebrew](https://brew.sh/) installation for [IBM Techzone](https://github.com/cloud-native-toolkit/atkcli-release)
+
+#### Installation
+
+To install the latest version of IBM Techzone into `/usr/local/bin`, run the following:
+
+```shell
+brew tap cloud-native-toolkit/homebrew-techzone
+brew install iascable
+```
+
+
+### Change Log
+
+- **11/2021** - Updated to use Client-to-site VPN service (beta) instead of a VSI running a VPN server
+- **11/2021** - Updated to support the Edge VPC infrastructure in addition to Management and Workload VPCs.
+
---
+++ atk.rb
@@ -0,0 +1,17 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class atk < Formula
+    desc "The atk command line interface is a scriptable command line interface that provides CLI access to IBM TechZone."
+    homepage ""
+    url "https://github.com/cloud-native-toolkit/atkcli-release/releases/download/v0.1.11/atkcli-darwin-amd64.tar.gz"
+    sha256 "0aed0716f1a8023242ba3fd6c0b1b9c819b27657b6dedb7d885a95740f421c0e"
+    license ""
+  
+    # depends_on "cmake" => :build
+    def install
+        bin.install 'atk'
+    end
+  
+  end
+  
