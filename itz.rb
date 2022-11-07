6efd9a13c6385b659290e202fb055ecf54d003e6
Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>
2022-11-07 15:00:35 -0500

    new version plus change name to itz

--- itz.rb
+++ itz.rb
@@ -1,8 +1,8 @@
 class Itz < Formula
     desc "The itz command line interface is a scriptable command line interface that provides CLI access to IBM TechZone."
     homepage ""
-    url "https://github.com/cloud-native-toolkit/itzcli-release/releases/download/v0.1.17/itzcli-darwin-amd64.tar.gz"
-    sha256 "90e2d8722b09d82f00a0c77447542879addaf3c6d88f5467e2ee812a5e5df6c0"
+    url "https://github.com/cloud-native-toolkit/itzcli-release/releases/download/v0.1.18/itzcli-darwin-amd64.tar.gz"
+    sha256 "9a2c17c4b19a1f739184b64c79eb53518b941bf98b0f475eff1c0b744c633a7a"
     license ""
   
     # depends_on "cmake" => :build
