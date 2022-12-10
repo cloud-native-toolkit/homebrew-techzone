18bef485c94a9e11a8df8b5939f978d440b34a33
David Stacy <davidstacy@users.noreply.github.com>
2022-12-10 10:04:54 -0500

    Update itz.rb

--- itz.rb
+++ itz.rb
@@ -1,6 +1,7 @@
 class Itz < Formula
     desc "The itz command line interface is a scriptable command line interface that provides CLI access to IBM TechZone."
     homepage ""
+    version "1.22"
     url "https://github.com/cloud-native-toolkit/itzcli-release/releases/download/v0.1.22/itzcli-darwin-amd64.tar.gz"
     sha256 "35a74784ad33806b68342466ca396bbfeb727d0a1d5b9f640f43d6342b8be25e"
     license ""
