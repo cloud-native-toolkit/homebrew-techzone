ec976a854b4db7be9993cc421720fa59324ea50d
Nathan Good <nathan.good@ibm.com>
2023-07-17 10:45:43 -0500

    Creating release v0.1.26
    Signed-off-by: Nathan Good <nathan.good@ibm.com>

--- itz.rb
+++ itz.rb
@@ -1,8 +1,8 @@
 class Itz < Formula
     desc "The itz command line interface is a scriptable command line interface that provides CLI access to IBM TechZone."
     homepage "https://github.com/cloud-native-toolkit/itzcli"
-    version "v0.1.25"
-    url "https://github.com/cloud-native-toolkit/itzcli/archive/refs/tags/v0.1.25.tar.gz"
+    version "v0.1.26"
+    url "https://github.com/cloud-native-toolkit/itzcli/archive/refs/tags/v0.1.26.tar.gz"
     sha256 "63c959f50e9773bff98b8667531e3d14f3d243d37111b7fb030bd246b1430b9b"
     license "Apache"
 
